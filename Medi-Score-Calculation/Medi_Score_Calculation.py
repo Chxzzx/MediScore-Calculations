833ffbe0965dd8c63d40be0639cbae396a8f6444
Chxzzx <charleybartle@gmail.com>
2025-03-05 14:07:03 +0000

    updated main to check if patient's score has increased by 2 in 24hr period

--- Medi-Score-Calculation/Medi_Score_Calculation.py
+++ Medi-Score-Calculation/Medi_Score_Calculation.py
@@ -1,5 +1,10 @@
 from enum import Enum
+import os
 from typing import Any
+from datetime import datetime
+
+##test data to be used (change through each test)
+
 
 class MediScore:
     score = 0
@@ -97,7 +102,6 @@ def processTestData(airOxygenVal, conscious, respirationValue, bloodOxygenValue,
     airOxygen = AirOxygen[airOxygenVal]  
     consciousness = Consciousness[conscious]
 
-
     ##initialises the objects
     respirationRange = RespirationRange()
     bloodOxygen = BloodOxygen()
@@ -122,15 +126,61 @@ def processTestData(airOxygenVal, conscious, respirationValue, bloodOxygenValue,
     MediScore.updateScore(consciousness.value)
     MediScore.updateScore(airOxygen.value)
 
+    print("Total MediScore:", MediScore.score)
+    return (MediScore.score)
+
 def main():
+    name = input("Enter patient's full name: ")
+    firstName, surName = name.split()
+    firstName = firstName.capitalize()
+    surName = surName.capitalize()
+    name = firstName+surName
+
     airOxygen = input("Enter air or oxygen: ").lower()
     conscious = input("Enter alert or CVPU: ").lower()
     respirationValue = int(input("Enter respiration range: "))
     bloodOxygenValue = int(input("Enter blood oxygen %: "))
     temperatureValue = float(input("Enter temperature: "))
-    processTestData(airOxygen, conscious, respirationValue, bloodOxygenValue, temperatureValue)
-    print("Total MediScore:", MediScore.score)
+    finalScore = processTestData(airOxygen, conscious, respirationValue, bloodOxygenValue, temperatureValue)
+
+    fileName = name+".txt"
+    currentDate = datetime.now()
+    
+    ##checks if file exists
+    if os.path.exists(fileName):
+        mostRecent = ''
+
+        ##fetches previous entry
+        with open (fileName, 'r') as readFile:
+            lines = readFile.readlines()
+            if lines:
+                mostRecent = lines[-1].strip()
+            readFile.close()
+        ##writes new data to file
+        with open(fileName, 'a') as patientFile:  
+            patientFile.write("\n" + str(finalScore) + " " + str(currentDate))
+
+        ##parses the data from previous entry
+        previousScore, previousDate, previousTime = mostRecent.split()
+        previousScore = int(previousScore)  
+        previousDate = datetime.strptime(previousDate, "%Y-%m-%d")
+        previousTime = datetime.strptime(previousTime, "%H:%M:%S").time()
+        previousDateTime = datetime.combine(previousDate, previousTime)
+
+        ##clalculates the time difference
+        timeDifference = currentDate - previousDateTime
+
+        ##pops the alert if score has increased in 24hr period
+        if finalScore >= previousScore + 2 and timeDifference.total_seconds() <= 86400:  
+                print("Alert: Score has increased by 2 in the last 24 hours!")
+    
+    ##if file doesn't exist, a file is created and data entered
+    else:
+        with open(fileName, 'w') as patientFile:
+             patientFile.write(str(finalScore) + " " + str(currentDate))
+
+
+
+
 
 main()
-
-
